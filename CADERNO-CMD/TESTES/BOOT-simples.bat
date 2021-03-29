87f7798eb752b6641456a6c8b658d2aaf255590f
cristiano <britonano@gmail.com>
2021-03-29 14:29:59 -0300

    Add files via upload

---
+++ CADERNO-CMD/TESTES/BOOT-simples.bat
@@ -0,0 +1,24 @@
+@echo off
+title DECLARANDO VARIAVEIS
+color 03
+mode 50,30
+
+set /p nome="DIGITE SEU NOME: "
+
+echo OLA %nome%
+echo seja bem vindo
+echo %nome% e um nome bonito
+echo.
+
+set /p nmae="DIGITE O NOME DA SUA MAE: "
+
+echo HUM O NOME DA SUA LINDA MAE E: %nmae%
+echo SEJA BEM VINDA DONA %nmae%
+echo: 
+
+set /p npai="DIGITE O NOME DO SEU PAI: "
+echo HUM O NOME DO SEU PAI E: %npai%
+echo SEJA BEM VINDO %npai%
+echo.
+
+pause >nul
